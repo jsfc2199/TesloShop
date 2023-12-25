c9c968496590a7a7be18b4bfd7f4bcb0fa940e7f
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-25 17:47:05 -0500

    TypeORM
    Se añade before insert para control del slug

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -42,4 +42,11 @@ export class Product {
 
   //tags
   //images
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) this.slug = this.title;
+
+    this.slug = this.slug.toLowerCase().replaceAll(' ', '_').replace("'", '');
+  }
 }
