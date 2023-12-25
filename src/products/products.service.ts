ca3896d87d7345263a5da7a30e1c488617d4f557
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-25 18:06:43 -0500

    TypeORM
    Se añade findAll, findOne y remove

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -33,20 +34,27 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  //TODO: Paginar
+  async findAll() {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({
+      id: id,
+    });
+    if (!product)
+      throw new NotFoundException(`Product with id: ${id} not found`);
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
   }
 
   private handleDBExceptions(error: any) {
