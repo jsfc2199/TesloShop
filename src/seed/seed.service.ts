3ce4d82172212849b124fc50f54574cf82a36bc6
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-01-25 19:40:40 -0500

    TypeORM Relaciones
    Se termina seed implementando la insercion de la data

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -11,6 +12,16 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productsService.deleteAllProducts();
+
+    //insertamos la informacion de forma masiva
+    const seedProducts = initialData.products;
+
+    const insertPromises = [];
+    seedProducts.forEach((product) => {
+      insertPromises.push(this.productsService.create(product)); //usamos el creat del propio servicio para reutilizar la lógica
+    });
+
+    await Promise.all(insertPromises); //esperamos a que todas las promesas se resuelvan. Hasta que no se cumplan no segura con la siguiente linea
     return true;
   }
 }
