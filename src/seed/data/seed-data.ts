d030f48e185eb9a395750f74d43f0bba2f014314
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-02-09 19:03:07 -0500

    Autenticacion
    Se usa bcrypt para encriptar las contraseñas del seed

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 interface SeedProduct {
   description: string;
   images: string[];
@@ -31,13 +32,13 @@ export const initialData: SeedData = {
     {
       email: 'test1@gmail.com',
       fullName: 'test one',
-      password: 'Abc123*',
+      password: bcrypt.hashSync('Abc123*', 10),
       roles: ['admin'],
     },
     {
       email: 'test2@gmail.com',
       fullName: 'test two',
-      password: 'Abc123*',
+      password: bcrypt.hashSync('Abc123*', 10),
       roles: ['user', 'super-user'],
     },
   ],
