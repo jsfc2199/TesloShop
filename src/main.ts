016f827b916bdfe4550cd8f3adeba1bac17b1fa0
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-02-11 12:26:25 -0500

    Documentacion
    Se setea configuracion inicial para documentacion de la api con swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Testo RestAPI')
+    .setDescription('Teslo shop endpoints description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); //endpoint api crea la app y la documentacion
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
