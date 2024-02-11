729b1a1500d8e8ee2aaa8677f171490996caa4f3
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-02-11 12:59:37 -0500

    Documentacion
    Se expande sobre la documentacion de dtos

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'how many rows do you need',
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number) //es lo mismo que el enable implicit conversions pero directo en el dto
   limit?: number;
 
+  @ApiProperty({
+    default: 2,
+    description: 'how many rows do you want to skip',
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number)
