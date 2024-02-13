584c67ba843e8c6b0297b284fa85d041c48d9321
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2024-02-12 20:38:37 -0500

    WebSockets
    Se añade uso de on gate connection y disconnet y se da uso en un frontend vanilla

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessageWsGateway {
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messageWsService: MessageWsService) {}
+  handleConnection(client: Socket) {
+    console.log(client.id, 'cliente conectado');
+  }
+  handleDisconnect(client: Socket) {
+    console.log(client.id, 'cliente desconectado');
+  }
 }
